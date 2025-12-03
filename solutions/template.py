89aa0c764ad1f53733d328b89a3c79944dc33a4c
Mohammad Abdul Quddus Salam <quddussalam555@gmail.com>
2025-12-03 07:49:29 +0700

    Feat: added type hints for template.py

--- solutions/template.py
+++ solutions/template.py
@@ -1,44 +1,46 @@
 import os
 
-DAY = 0  # Fill in the day.
+DAY: int = 0  # Fill in the day.
 
 
-def main():
+def main() -> None:
     try:
-        script_dir = os.path.dirname(os.path.abspath(__file__))
-        data_path = os.path.join(script_dir, "data.txt")
-
-        with open(data_path) as f:
-            data = [line.strip() for line in f.readlines()]
-
-            print("ADVENT OF CODE 2025")
-            print("Copyright (C) XnonXte 2025")
-            print("=================================================")
-
-            part_1_answer, part_2_answer = solution(data)
-
-            print(f"Day {DAY} answers:")
-            print(f"Part 1: {part_1_answer}")
-            print(f"Part 2: {part_2_answer}")
-
+        script_dir: str = os.path.dirname(os.path.abspath(__file__))
+        data_path: str = os.path.join(script_dir, "data.txt")
+        data: list[str] = read_data(data_path) 
+        part_1_answer: int = part_1(data)
+        part_2_answer: int = part_2(data)
+        print_answers(part_1_answer, part_2_answer)
     except FileNotFoundError:
         print("Data file not found!")
     except Exception as e:
         print(f"Error: {e}")
+       
+        
+def print_answers(part_1_answer: int, part_2_answer: int) -> None:
+        print("ADVENT OF CODE 2025")
+        print("Copyright (C) XnonXte 2025")
+        print("=================================================")
+        print(f"Day {DAY} answers:")
+        print(f"Part 1: {part_1_answer}")
+        print(f"Part 2: {part_2_answer}")
+      
+        
+def read_data(data_path: str) -> list[str]:
+    with open(data_path) as f:
+        return [line.strip() for line in f.readlines()]
+    
 
-
-def solution(data):
-    return part_1(data), part_2(data)
-
-
-def part_1(data):
+def part_1(data: list[str]) -> int:
     # TODO: implement part 1
-    return None
+    answer: int = 0
+    return answer 
 
 
-def part_2(data):
-    # TODO: implement part 2
-    return None
+def part_2(data: list[str]) -> int:
+    # TODO: implement part 2 
+    answer: int = 0
+    return answer 
 
 
 if __name__ == "__main__":
