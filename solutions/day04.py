5dbcd684660ae3d775f04d4ccf923df9da319e0e
Mohammad Abdul Quddus Salam <quddussalam555@gmail.com>
2025-12-04 18:21:47 +0700

    Feat: Day 4 part 1 and 2

---
+++ solutions/day04.py
@@ -0,0 +1,98 @@
+import os
+
+DAY: int = 4  # Fill in the day.
+
+
+def main() -> None:
+    try:
+        script_dir: str = os.path.dirname(os.path.abspath(__file__))
+        data_path: str = os.path.join(script_dir, "data.txt")
+        data: list[list[str]] = [list(line) for line in read_data(data_path)] # Day 4 only fix.
+        part_1_answer: int = part_1(data)
+        part_2_answer: int = part_2(data)
+        print_answers(part_1_answer, part_2_answer)
+    except FileNotFoundError:
+        print("Data file not found!")
+    except Exception as e:
+        print(f"Error: {e}")
+
+
+def print_answers(part_1_answer: int, part_2_answer: int) -> None:
+    print("ADVENT OF CODE 2025")
+    print("Copyright (C) XnonXte 2025")
+    print("=================================================")
+    print(f"Day {DAY} answers:")
+    print(f"Part 1: {part_1_answer}")
+    print(f"Part 2: {part_2_answer}")
+
+
+def read_data(data_path: str) -> list[str]:
+    with open(data_path) as f:
+        return [line.strip() for line in f.readlines()]
+
+
+def check_if_safe(data: list[list[str]], i: int, j: int) -> bool:
+    try:
+        if (i < 0 or j < 0):
+            return False 
+        return data[i][j] == "@"
+    except IndexError:
+        return False
+    
+    
+def invert_paper(data: list[list[str]]) -> tuple[list[list[str]], int]: 
+    total_inverted = 0
+    for i in range(len(data)):
+        for j in range(len(data[i])):
+            if data[i][j] == "@":
+                indexes_pair = [
+                [i + 1, j], 
+                [i, j + 1], 
+                [i - 1 , j], 
+                [i, j - 1], 
+                [i + 1, j - 1], 
+                [i - 1, j + 1], 
+                [i + 1, j + 1], 
+                [i - 1, j - 1], 
+                ]
+                eight_adjacent_checks = list(check_if_safe(data, i, j) for i, j in indexes_pair) 
+                if (sum(eight_adjacent_checks) < 4):
+                    data[i][j] = "."
+                    total_inverted += 1
+    return data, total_inverted
+
+
+def part_1(data: list[list[str]]) -> int:
+    answer: int = 0
+    for i in range(len(data)):
+        for j in range(len(data[i])):
+            if data[i][j] == "@":
+                indexes_pair = [
+                [i + 1, j], 
+                [i, j + 1], 
+                [i - 1 , j], 
+                [i, j - 1], 
+                [i + 1, j - 1], 
+                [i - 1, j + 1], 
+                [i + 1, j + 1], 
+                [i - 1, j - 1], 
+                ]
+                eight_adjacent_checks = list(check_if_safe(data, i, j) for i, j in indexes_pair) 
+                if (sum(eight_adjacent_checks) < 4):
+                    answer += 1
+    return answer
+
+def part_2(data: list[list[str]]) -> int: 
+   answer: int = 0
+   while True:
+       data, total_inverted = invert_paper(data)
+       if (total_inverted == 0):
+           break
+       else:
+           answer += total_inverted
+   return answer
+
+
+
+if __name__ == "__main__":
+    main()
